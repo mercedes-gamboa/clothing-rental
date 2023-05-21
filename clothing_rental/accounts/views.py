a7e443366783cf907a06dc872069a6658a7e7f24
mercedes-gamboa <115371746+mercedes-gamboa@users.noreply.github.com>
2023-05-21 14:51:09 +0200

    Create registration for costomized user model

--- clothing_rental/accounts/views.py
+++ clothing_rental/accounts/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login
+from .forms import UserRegistrationForm
 
 # Create your views here.
-def display(request):
-    s = '<h1>Hello! welcome to RentB !</h1>'
-    return HttpResponse(s)
+
+def register(request):
+     if request.user.is_authenticated:
+         return redirect('/')
+
+     if request.method == "POST":
+         form = UserRegistrationForm(request.POST)
+         if form.is_valid():
+             user = form.save()
+             login(request, user)
+             return redirect("/")
+
+         else:
+             for error in list(form.errors.values()):
+                 print(request, error)
+
+     else:
+         form = UserRegistrationForm()
+
+     return render(
+         request=request,
+         template_name="register.html",
+         context={"form":form}
+     )
