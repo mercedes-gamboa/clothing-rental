7f2f5ec82e8fabc0085d78eb9de4d1de3a4d711d
mercedes-gamboa <115371746+mercedes-gamboa@users.noreply.github.com>
2023-05-21 15:36:28 +0200

    Update settings.py

--- clothing_rental/clothing_rental/settings.py
+++ clothing_rental/clothing_rental/settings.py
@@ -19,7 +19,7 @@ import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+TEMPLATE_DIR = BASE_DIR / "templates"
 
 dotenv.load_dotenv()
 # Quick-start development settings - unsuitable for production
